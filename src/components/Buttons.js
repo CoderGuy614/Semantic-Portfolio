7d0153d9e9d83a16d872fd565a078f93995f46d9
CoderGuy614 <jlutz.110@gmail.com>
2020-09-21 12:51:19 +0800

    Initial Commit

---
+++ src/components/Buttons.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { Button } from "semantic-ui-react";
+
+const ButtonExampleButton = () => <Button primary>Click Here</Button>;
+
+export default ButtonExampleButton;
