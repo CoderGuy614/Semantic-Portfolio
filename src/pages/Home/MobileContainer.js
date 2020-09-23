40deb96b3f970eeeb7184d09673d9a12a1d01e45
CoderGuy614 <jlutz.110@gmail.com>
2020-09-23 08:23:29 +0800

    Add NavLinks to MobileContainer

--- src/pages/Home/MobileContainer.js
+++ src/pages/Home/MobileContainer.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { NavLink } from "react-router-dom";
 import PropTypes from "prop-types";
 
 import { Container, Sidebar, Menu, Segment, Icon } from "semantic-ui-react";
@@ -6,9 +7,34 @@ import HomepageHeading from "../../components/HomepageHeading";
 
 const MobileContainer = ({ children, Media }) => {
   const [sidebarOpened, setSidebarOpened] = useState(false);
+  const [activeItem, setActiveItem] = useState("");
   const handleToggle = () => {
     setSidebarOpened(true);
   };
+
+  const handleItemClick = (e, { name }) => setActiveItem(name);
+
+  useEffect(() => {
+    if (window.location.pathname === "/" && activeItem !== "home") {
+      setActiveItem("home");
+    } else if (
+      window.location.pathname === "/about" &&
+      activeItem !== "about"
+    ) {
+      setActiveItem("about");
+    } else if (
+      window.location.pathname === "/projects" &&
+      activeItem !== "projects"
+    ) {
+      setActiveItem("projects");
+    } else if (
+      window.location.pathname === "/contacts" &&
+      activeItem !== "contacts"
+    ) {
+      setActiveItem("contacts");
+    }
+  }, [activeItem]);
+
   return (
     <Media as={Sidebar.Pushable} at="mobile">
       <Sidebar.Pushable>
@@ -20,12 +46,46 @@ const MobileContainer = ({ children, Media }) => {
           vertical
           visible={sidebarOpened}
         >
-          <Menu.Item as="a" active>
+          <Menu.Item
+            name="home"
+            active={activeItem === "home"}
+            onClick={handleItemClick}
+            as={NavLink}
+            to="/"
+            exact
+          >
             Home
           </Menu.Item>
-          <Menu.Item as="a">Work</Menu.Item>
-          <Menu.Item as="a">Company</Menu.Item>
-          <Menu.Item as="a">Careers</Menu.Item>
+          <Menu.Item
+            name="projects"
+            active={activeItem === "projects"}
+            onClick={handleItemClick}
+            as={NavLink}
+            to="/projects"
+            exact
+          >
+            Projects
+          </Menu.Item>
+          <Menu.Item
+            name="about"
+            active={activeItem === "about"}
+            onClick={handleItemClick}
+            as={NavLink}
+            to="/about"
+            exact
+          >
+            About
+          </Menu.Item>
+          <Menu.Item
+            name="contact"
+            active={activeItem === "contact"}
+            onClick={handleItemClick}
+            as={NavLink}
+            to="contact"
+            exact
+          >
+            Contact
+          </Menu.Item>
         </Sidebar>
 
         <Sidebar.Pusher dimmed={sidebarOpened}>
