8cdb1a919e6ed089d8d72cd55b58a1ad5bb44463
CoderGuy614 <jlutz.110@gmail.com>
2020-09-26 17:42:27 +0800

    Add Content to Projects Page

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,7 +1,11 @@
 import React from "react";
-import { Item, Image } from "semantic-ui-react";
+import { Item, Image, Container, Divider, Icon } from "semantic-ui-react";
 import PageHeader from "../components/PageHeader";
 import Footer from "../components/Footer";
+
+const itemStyle = {
+  margin: "20px 0px",
+};
 
 const Projects = () => {
   return (
@@ -11,6 +15,89 @@ const Projects = () => {
         mainText="Projects"
         subText="Check Out Some of My Recent Work"
       />
+      <Container style={{ position: "relative" }}>
+        <Item.Group style={{ position: "relative" }}>
+          {/* Project 1 LocalBiz */}
+          <Item style={itemStyle}>
+            <Item.Image
+              size="large"
+              src={require("../img/projects/LocalBiz/project_7_1.png")}
+            />
+
+            <Item.Content>
+              <Item.Header as="a">LocalBiz</Item.Header>
+              <Item.Meta>Small and Local Business Directory</Item.Meta>
+              <Item.Description>
+                <Image src="https://react.semantic-ui.com/images/wireframe/short-paragraph.png" />
+              </Item.Description>
+              <Item.Extra>Additional Details</Item.Extra>
+            </Item.Content>
+          </Item>
+
+          <Divider horizontal>
+            <Icon name="star" color="yellow" />
+          </Divider>
+          {/* Project 2 VeggieKing */}
+          <Item style={itemStyle}>
+            <Item.Image
+              size="large"
+              src={require("../img/projects/VeggieKing/project_1_2.png")}
+            />
+
+            <Item.Content>
+              <Item.Header as="a">VeggieKing</Item.Header>
+              <Item.Meta>Food Ordering and Delivery</Item.Meta>
+              <Item.Description>
+                <Image src="https://react.semantic-ui.com/images/wireframe/short-paragraph.png" />
+              </Item.Description>
+              <Item.Extra>Additional Details</Item.Extra>
+            </Item.Content>
+          </Item>
+          <Divider horizontal>
+            <Icon name="star" color="yellow" />
+          </Divider>
+          {/* Project 3 RealEstate */}
+          <Item style={itemStyle}>
+            <Item.Image
+              size="large"
+              src={require("../img/projects/RealEstate/project_6_2.png")}
+            />
+
+            <Item.Content>
+              <Item.Header as="a">Hummell & Long Real Estate</Item.Header>
+              <Item.Meta>Real Estate Listings</Item.Meta>
+              <Item.Description>
+                <Image src="https://react.semantic-ui.com/images/wireframe/short-paragraph.png" />
+              </Item.Description>
+              <Item.Extra>Additional Details</Item.Extra>
+            </Item.Content>
+          </Item>
+
+          <Divider horizontal>
+            <Icon name="star" color="yellow" />
+          </Divider>
+          {/* Project 4 CamboCraft */}
+          <Item style={itemStyle}>
+            <Item.Image
+              size="large"
+              src={require("../img/projects/CamboCraft/project_2_1.png")}
+            />
+
+            <Item.Content>
+              <Item.Header as="a">CamboCraft</Item.Header>
+              <Item.Meta>Custom Clothing Online Store</Item.Meta>
+              <Item.Description>
+                <Image src="https://react.semantic-ui.com/images/wireframe/short-paragraph.png" />
+              </Item.Description>
+              <Item.Extra>Additional Details</Item.Extra>
+            </Item.Content>
+          </Item>
+
+          <Divider horizontal>
+            <Icon name="star" color="yellow" />
+          </Divider>
+        </Item.Group>
+      </Container>
       <Footer />
     </div>
   );
