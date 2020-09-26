63038a082b82b9b59d30bc07db310cca44a45ded
CoderGuy614 <jlutz.110@gmail.com>
2020-09-26 16:04:16 +0800

    Add Icon to Home and Adjust Formatting

--- src/components/TechIcons.js
+++ src/components/TechIcons.js
@@ -7,18 +7,23 @@ const iconContainerStyle = {
   justifyContent: "center",
 };
 
-const iconStyle = {
+const iconStyleS = {
   maxWidth: "50px",
   margin: "1em",
 };
 
-const iconStyleBack = {
+const iconStyleM = {
   maxWidth: "75px",
   margin: "1em",
 };
 
-const iconStyleDev = {
+const iconStyleL = {
   maxWidth: "100px",
+  margin: "1em",
+};
+
+const iconStyleXL = {
+  maxWidth: "125px",
   margin: "1em",
 };
 
@@ -30,31 +35,31 @@ const TechIcons = () => {
         Front End Technologies
       </Header>
       <Segment style={iconContainerStyle}>
-        <Image style={iconStyle} src={require("../img/html.png")} />
-        <Image style={iconStyle} src={require("../img/css.png")} />
-        <Image style={iconStyle} src={require("../img/js.png")} />
-        <Image style={iconStyle} src={require("../img/react2.jpg")} />
-        <Image style={iconStyle} src={require("../img/sass.png")} />
-        <Image style={iconStyle} src={require("../img/reactBootstrap.png")} />
+        <Image style={iconStyleS} src={require("../img/html.png")} />
+        <Image style={iconStyleS} src={require("../img/css.png")} />
+        <Image style={iconStyleS} src={require("../img/js.png")} />
+        <Image style={iconStyleS} src={require("../img/react2.jpg")} />
+        <Image style={iconStyleS} src={require("../img/sass.png")} />
+        <Image style={iconStyleS} src={require("../img/reactBootstrap.png")} />
       </Segment>
       <Header className="primaryColor" textAlign="center">
         {" "}
         Back End Technologies
       </Header>
       <Segment style={iconContainerStyle}>
-        <Image style={iconStyleBack} src={require("../img/mongo.png")} />
-        <Image style={iconStyleBack} src={require("../img/mysql.png")} />
-        <Image style={iconStyleBack} src={require("../img/node.png")} />
-        <Image style={iconStyleDev} src={require("../img/express.png")} />
+        <Image style={iconStyleL} src={require("../img/mongo.png")} />
+        <Image style={iconStyleM} src={require("../img/mysql.png")} />
+        <Image style={iconStyleM} src={require("../img/node.png")} />
+        <Image style={iconStyleL} src={require("../img/express.png")} />
       </Segment>
       <Header className="primaryColor" textAlign="center">
         Development Tools
       </Header>
       <Segment style={iconContainerStyle}>
-        <Image style={iconStyleDev} src={require("../img/postman.png")} />
-        <Image style={iconStyleDev} src={require("../img/git.png")} />
-
-        <Image style={iconStyle} src={require("../img/terminal.png")} />
+        <Image style={iconStyleL} src={require("../img/postman.png")} />
+        <Image style={iconStyleL} src={require("../img/git.png")} />
+        <Image style={iconStyleS} src={require("../img/terminal.png")} />
+        <Image style={iconStyleXL} src={require("../img/vscode.png")} />
       </Segment>
     </>
   );
