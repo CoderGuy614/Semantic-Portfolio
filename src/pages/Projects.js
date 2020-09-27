7e0028cedfdeb34e9906533824a0871327ac8fe9
CoderGuy614 <jlutz.110@gmail.com>
2020-09-27 17:32:17 +0800

    Add Content to Projects Page

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,10 +1,42 @@
 import React from "react";
-import { Item, Image, Container, Divider, Icon } from "semantic-ui-react";
+import {
+  Item,
+  Image,
+  Container,
+  Divider,
+  Icon,
+  Segment,
+  Button,
+} from "semantic-ui-react";
 import PageHeader from "../components/PageHeader";
 import Footer from "../components/Footer";
 
+const iconContainerStyle = {
+  display: "flex",
+  flexWrap: "wrap",
+  justifyContent: "center",
+  padding: "5px",
+};
+
+const buttonStyle = {
+  display: "flex",
+  flexWrap: "wrap",
+  justifyContent: "space-around",
+  padding: "5px",
+};
+
 const itemStyle = {
   margin: "20px 0px",
+};
+
+const iconStyleXS = {
+  maxWidth: "20px",
+  margin: ".5em",
+};
+
+const iconStyleS = {
+  maxWidth: "40px",
+  margin: ".5em",
 };
 
 const Projects = () => {
@@ -28,9 +60,42 @@ const Projects = () => {
               <Item.Header as="a">LocalBiz</Item.Header>
               <Item.Meta>Small and Local Business Directory</Item.Meta>
               <Item.Description>
-                <Image src="https://react.semantic-ui.com/images/wireframe/short-paragraph.png" />
+                <p>
+                  This full-stack MERN application allows users post a business,
+                  edit their business info, list items for sale, and send and
+                  receive messages from other users.
+                </p>
               </Item.Description>
-              <Item.Extra>Additional Details</Item.Extra>
+              <Item.Extra>Technologies used:</Item.Extra>
+              <Segment compact style={iconContainerStyle}>
+                <Image style={iconStyleXS} src={require("../img/html.png")} />
+                <Image style={iconStyleXS} src={require("../img/css.png")} />
+                <Image style={iconStyleXS} src={require("../img/js.png")} />
+                <Image style={iconStyleXS} src={require("../img/react2.jpg")} />
+                <Image
+                  style={iconStyleXS}
+                  src={require("../img/reactBootstrap.png")}
+                />
+                <Image style={iconStyleS} src={require("../img/node.png")} />
+                <Image style={iconStyleS} src={require("../img/express.png")} />
+                <Image style={iconStyleS} src={require("../img/mongo.png")} />
+              </Segment>
+              <div style={buttonStyle}>
+                <Button
+                  primary
+                  size="large"
+                  content="View App"
+                  icon="eye"
+                  labelPosition="left"
+                />
+                <Button
+                  secondary
+                  size="large"
+                  content="Github Repo"
+                  icon="github"
+                  labelPosition="left"
+                />
+              </div>
             </Item.Content>
           </Item>
 
@@ -48,11 +113,45 @@ const Projects = () => {
               <Item.Header as="a">VeggieKing</Item.Header>
               <Item.Meta>Food Ordering and Delivery</Item.Meta>
               <Item.Description>
-                <Image src="https://react.semantic-ui.com/images/wireframe/short-paragraph.png" />
+                <p>
+                  This full-stack MERN application allows users post a business,
+                  edit their business info, list items for sale, and send and
+                  receive messages from other users.
+                </p>
               </Item.Description>
-              <Item.Extra>Additional Details</Item.Extra>
+              <Item.Extra>Technologies used:</Item.Extra>
+              <Segment compact style={iconContainerStyle}>
+                <Image style={iconStyleXS} src={require("../img/html.png")} />
+                <Image style={iconStyleXS} src={require("../img/css.png")} />
+                <Image style={iconStyleXS} src={require("../img/js.png")} />
+                <Image style={iconStyleXS} src={require("../img/react2.jpg")} />
+                <Image
+                  style={iconStyleXS}
+                  src={require("../img/reactBootstrap.png")}
+                />
+                <Image style={iconStyleS} src={require("../img/node.png")} />
+                <Image style={iconStyleS} src={require("../img/express.png")} />
+                <Image style={iconStyleS} src={require("../img/mongo.png")} />
+              </Segment>
+              <div style={buttonStyle}>
+                <Button
+                  primary
+                  size="large"
+                  content="View App"
+                  icon="eye"
+                  labelPosition="left"
+                />
+                <Button
+                  secondary
+                  size="large"
+                  content="Github Repo"
+                  icon="github"
+                  labelPosition="left"
+                />
+              </div>
             </Item.Content>
           </Item>
+
           <Divider horizontal>
             <Icon name="star" color="yellow" />
           </Divider>
@@ -67,9 +166,42 @@ const Projects = () => {
               <Item.Header as="a">Hummell & Long Real Estate</Item.Header>
               <Item.Meta>Real Estate Listings</Item.Meta>
               <Item.Description>
-                <Image src="https://react.semantic-ui.com/images/wireframe/short-paragraph.png" />
+                <p>
+                  This full-stack MERN application allows users post a business,
+                  edit their business info, list items for sale, and send and
+                  receive messages from other users.
+                </p>
               </Item.Description>
-              <Item.Extra>Additional Details</Item.Extra>
+              <Item.Extra>Technologies used:</Item.Extra>
+              <Segment compact style={iconContainerStyle}>
+                <Image style={iconStyleXS} src={require("../img/html.png")} />
+                <Image style={iconStyleXS} src={require("../img/css.png")} />
+                <Image style={iconStyleXS} src={require("../img/js.png")} />
+                <Image style={iconStyleXS} src={require("../img/react2.jpg")} />
+                <Image
+                  style={iconStyleXS}
+                  src={require("../img/reactBootstrap.png")}
+                />
+                <Image style={iconStyleS} src={require("../img/node.png")} />
+                <Image style={iconStyleS} src={require("../img/express.png")} />
+                <Image style={iconStyleS} src={require("../img/mongo.png")} />
+              </Segment>
+              <div style={buttonStyle}>
+                <Button
+                  primary
+                  size="large"
+                  content="View App"
+                  icon="eye"
+                  labelPosition="left"
+                />
+                <Button
+                  secondary
+                  size="large"
+                  content="Github Repo"
+                  icon="github"
+                  labelPosition="left"
+                />
+              </div>
             </Item.Content>
           </Item>
 
@@ -87,9 +219,42 @@ const Projects = () => {
               <Item.Header as="a">CamboCraft</Item.Header>
               <Item.Meta>Custom Clothing Online Store</Item.Meta>
               <Item.Description>
-                <Image src="https://react.semantic-ui.com/images/wireframe/short-paragraph.png" />
+                <p>
+                  This full-stack MERN application allows users post a business,
+                  edit their business info, list items for sale, and send and
+                  receive messages from other users.
+                </p>
               </Item.Description>
-              <Item.Extra>Additional Details</Item.Extra>
+              <Item.Extra>Technologies used:</Item.Extra>
+              <Segment compact style={iconContainerStyle}>
+                <Image style={iconStyleXS} src={require("../img/html.png")} />
+                <Image style={iconStyleXS} src={require("../img/css.png")} />
+                <Image style={iconStyleXS} src={require("../img/js.png")} />
+                <Image style={iconStyleXS} src={require("../img/react2.jpg")} />
+                <Image
+                  style={iconStyleXS}
+                  src={require("../img/reactBootstrap.png")}
+                />
+                <Image style={iconStyleS} src={require("../img/node.png")} />
+                <Image style={iconStyleS} src={require("../img/express.png")} />
+                <Image style={iconStyleS} src={require("../img/mongo.png")} />
+              </Segment>
+              <div style={buttonStyle}>
+                <Button
+                  primary
+                  size="large"
+                  content="View App"
+                  icon="eye"
+                  labelPosition="left"
+                />
+                <Button
+                  secondary
+                  size="large"
+                  content="Github Repo"
+                  icon="github"
+                  labelPosition="left"
+                />
+              </div>
             </Item.Content>
           </Item>
 
@@ -107,15 +272,151 @@ const Projects = () => {
               <Item.Header as="a">E-Commerce Shop</Item.Header>
               <Item.Meta>Online Clothing Store</Item.Meta>
               <Item.Description>
-                <Image src="https://react.semantic-ui.com/images/wireframe/short-paragraph.png" />
+                <p>
+                  This full-stack MERN application allows users post a business,
+                  edit their business info, list items for sale, and send and
+                  receive messages from other users.
+                </p>
               </Item.Description>
-              <Item.Extra>Additional Details</Item.Extra>
+              <Item.Extra>Technologies used:</Item.Extra>
+              <Segment compact style={iconContainerStyle}>
+                <Image style={iconStyleXS} src={require("../img/html.png")} />
+                <Image style={iconStyleXS} src={require("../img/css.png")} />
+                <Image style={iconStyleXS} src={require("../img/js.png")} />
+                <Image style={iconStyleXS} src={require("../img/react2.jpg")} />
+                <Image
+                  style={iconStyleXS}
+                  src={require("../img/reactBootstrap.png")}
+                />
+                <Image style={iconStyleS} src={require("../img/node.png")} />
+                <Image style={iconStyleS} src={require("../img/express.png")} />
+                <Image style={iconStyleS} src={require("../img/mongo.png")} />
+              </Segment>
+              <div style={buttonStyle}>
+                <Button
+                  primary
+                  size="large"
+                  content="View App"
+                  icon="eye"
+                  labelPosition="left"
+                />
+                <Button
+                  secondary
+                  size="large"
+                  content="Github Repo"
+                  icon="github"
+                  labelPosition="left"
+                />
+              </div>
             </Item.Content>
           </Item>
 
           <Divider horizontal>
             <Icon name="star" color="yellow" />
           </Divider>
+          {/* Project 6 Spotify */}
+          <Item style={itemStyle}>
+            <Item.Image
+              size="large"
+              src={require("../img/projects/Spotify/project_4_2.jpg")}
+            />
+
+            <Item.Content>
+              <Item.Header as="a">Spotify</Item.Header>
+              <Item.Meta>Music Player</Item.Meta>
+              <Item.Description>
+                <p>
+                  This full-stack MERN application allows users post a business,
+                  edit their business info, list items for sale, and send and
+                  receive messages from other users.
+                </p>
+              </Item.Description>
+              <Item.Extra>Technologies used:</Item.Extra>
+              <Segment compact style={iconContainerStyle}>
+                <Image style={iconStyleXS} src={require("../img/html.png")} />
+                <Image style={iconStyleXS} src={require("../img/css.png")} />
+                <Image style={iconStyleXS} src={require("../img/js.png")} />
+                <Image style={iconStyleXS} src={require("../img/react2.jpg")} />
+                <Image
+                  style={iconStyleXS}
+                  src={require("../img/reactBootstrap.png")}
+                />
+                <Image style={iconStyleS} src={require("../img/node.png")} />
+                <Image style={iconStyleS} src={require("../img/express.png")} />
+                <Image style={iconStyleS} src={require("../img/mongo.png")} />
+              </Segment>
+              <div style={buttonStyle}>
+                <Button
+                  primary
+                  size="large"
+                  content="View App"
+                  icon="eye"
+                  labelPosition="left"
+                />
+                <Button
+                  secondary
+                  size="large"
+                  content="Github Repo"
+                  icon="github"
+                  labelPosition="left"
+                />
+              </div>
+            </Item.Content>
+          </Item>
+
+          <Divider horizontal>
+            <Icon name="star" color="yellow" />
+          </Divider>
+
+          {/* Project 7 AirBnB */}
+          <Item style={itemStyle}>
+            <Item.Image
+              size="large"
+              src={require("../img/projects/AirBnB/project_3_2.jpg")}
+            />
+
+            <Item.Content>
+              <Item.Header as="a">AirBnB</Item.Header>
+              <Item.Meta>Property Rental</Item.Meta>
+              <Item.Description>
+                <p>
+                  This full-stack MERN application allows users post a business,
+                  edit their business info, list items for sale, and send and
+                  receive messages from other users.
+                </p>
+              </Item.Description>
+              <Item.Extra>Technologies used:</Item.Extra>
+              <Segment compact style={iconContainerStyle}>
+                <Image style={iconStyleXS} src={require("../img/html.png")} />
+                <Image style={iconStyleXS} src={require("../img/css.png")} />
+                <Image style={iconStyleXS} src={require("../img/js.png")} />
+                <Image style={iconStyleXS} src={require("../img/react2.jpg")} />
+                <Image
+                  style={iconStyleXS}
+                  src={require("../img/reactBootstrap.png")}
+                />
+                <Image style={iconStyleS} src={require("../img/node.png")} />
+                <Image style={iconStyleS} src={require("../img/express.png")} />
+                <Image style={iconStyleS} src={require("../img/mongo.png")} />
+              </Segment>
+              <div style={buttonStyle}>
+                <Button
+                  primary
+                  size="large"
+                  content="View App"
+                  icon="eye"
+                  labelPosition="left"
+                />
+                <Button
+                  secondary
+                  size="large"
+                  content="Github Repo"
+                  icon="github"
+                  labelPosition="left"
+                />
+              </div>
+            </Item.Content>
+          </Item>
         </Item.Group>
       </Container>
       <Footer />
